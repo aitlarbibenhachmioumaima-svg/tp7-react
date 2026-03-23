eb4b546fdf07c5a4fcca6b8212ecada9ee5f16da
ait larbi ben hachmi oumaima <aitlarbibenhachmioumaima@gmail.com>
2026-03-23 20:01:00 +0100

    Ajout de modefication

--- src/DataLoader.js
+++ src/DataLoader.js
@@ -1,7 +1,24 @@
-function DataLoader({ render }) {
-  const users = ['Oumaima', 'Taha', 'Amin', 'Chaimae'];
+// src/DataLoader.js
+import React from "react";
 
-  return <div>{render(users)}</div>;
+// DataLoader stylé avec de nouveaux noms
+function DataLoader({ render }) {
+  const people = ["Oumaima", "Adam", "Amin"];
+
+  return (
+    <div
+      style={{
+        padding: "1rem",
+        backgroundColor: "#dff9fb",
+        borderRadius: "10px",
+        boxShadow: "0 3px 10px rgba(0,0,0,0.08)",
+        fontFamily: "'Arial', sans-serif",
+        marginBottom: "1rem",
+      }}
+    >
+      {render(people)}
+    </div>
+  );
 }
 
 export default DataLoader;
